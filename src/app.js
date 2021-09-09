69bb2b7ce1b36116bc5a4e6d11477cbcbb54177a
Ben Gates <benjamin_gates@outlook.com>
2021-09-08 22:40:57 -0600

    got server running properly

--- src/app.js
+++ src/app.js
@@ -5,3 +5,5 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+module.exports = app;
