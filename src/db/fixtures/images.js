ff56e737a6d21bd81faf38976f71ce48c0cc97b3
Ben Gates <benjamin_gates@outlook.com>
2021-09-15 22:55:47 -0600

    created seed data for images table

---
+++ src/db/fixtures/images.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        caption: "Galinthia amoena (Kenya flower) I2/3",
+        image_url: "https://imgur.com/HUU7CWt"    
+    },
+    {
+        caption: "Galinthia amoena (Kenya flower) I2/3",
+        image_url: "https://imgur.com/jx9sWmH"    
+    },
+    {
+        caption: "Pseudocreobotra wahlbergii (Spiny flower) I2/3",
+        image_url: "https://imgur.com/A0wQ8qv"    
+    },
+    {
+        caption: "Majangella moultoni (Giant moss mantis) I3/4",
+        image_url: "https://imgur.com/do3JW7f"    
+    }
+]
