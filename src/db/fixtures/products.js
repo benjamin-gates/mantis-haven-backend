71866906dec0f7b5a8a412923d6c0f18ac4c2ade
Ben Gates <benjamin_gates@outlook.com>
2021-09-15 23:13:35 -0600

    initiated data for products seed

---
+++ src/db/fixtures/products.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        product_name: "mantis",
+        product_url: "some url",
+        price: "some price integer",
+        image_id: "some image id"
+    }
+]
--- src/db/seeds/00-images.js
+++ src/db/seeds/00-images.js
@@ -1,10 +1,9 @@
-
+const images = require("../fixtures/images");
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex.raw("TRUNCATE TABLE images RESTART IDENTITY CASCADE")
     .then(function () {
       // Inserts seed entries
-      return knex('images').insert([
-      ]);
+      return knex('images').insert(images);
     });
 };
